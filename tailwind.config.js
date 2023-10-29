29e35a79d831ec1303b659678013dc4e2de81802
Thomas Bruand <thomas.bruand@gmail.com>
2023-10-29 10:34:21 +0100

    Ajout du fichier tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`
+    }
+    return `rgb(var(${variableName}))`
+  }
+}
+
+
+module.exports = {
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Rubik', 'sans-serif'],
+    },
+    extend: {
+      textColor: {
+        skin: {
+          base: withOpacity('--text-base'),
+          color: withOpacity('--text-color'),
+          input: withOpacity('--input'),
+          border: withOpacity('--border'),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          background: withOpacity('--background'),
+          foreground: withOpacity('--foreground'),
+          primary: withOpacity('--primary'),
+          secondary: withOpacity('--secondary'),
+          input: withOpacity('--input'),
+          border: withOpacity('--border'),
+        },
+      },      
+    },
+  },
+  plugins: [],
+}
